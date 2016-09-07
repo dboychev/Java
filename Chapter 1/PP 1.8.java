08cee84c35a7672f57e2581fd4a0afaf449e4af7
dboychev <dboychev95@gmail.com>
2016-09-07 17:10:52 +0300

    New files
    All Chapter 1 programs added.

---
+++ Chapter 1/PP 1.8.java
@@ -0,0 +1,18 @@
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("JJJJJJJJJJJJJJJ   AAAAAAAA    LLLL");
+		System.out.println("JJJJJJJJJJJJJJJ  AAAAAAAAAA   LLLL");
+		System.out.println("        JJJJ     AAA    AAA   LLLL");
+		System.out.println("        JJJJ     AAA    AAA   LLLL");
+		System.out.println("        JJJJ     AAAAAAAAAA   LLLL");
+		System.out.println("J       JJJJ     AAAAAAAAAA   LLLL");
+		System.out.println("JJ      JJJJ     AAA    AAA   LLLL");
+		System.out.println(" JJJJJJJJJJJ     AAA    AAA   LLLLLLLLLLLLLL");
+		System.out.println("  JJJJJJJJJ      AAA    AAA   LLLLLLLLLLLLLL");
+	}
+
+}
