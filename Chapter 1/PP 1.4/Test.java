2e5fb8387b663951f14a69e86c2ec07aefea8570
dboychev <dboychev95@gmail.com>
2016-09-07 17:07:17 +0300

    Second Java program
    PP 1.4 added.

---
+++ Chapter 1/PP 1.4/Test.java
@@ -0,0 +1,17 @@
+
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+		//1.4 a.
+		System.out.println("Knowledge is Power\n");
+		
+		//1.4 b.
+		System.out.println("Knowledge\n   is   \n  power  \n");
+		
+		//1.4 c.
+		System.out.println("===========\n|Knowledge|\n|   is    |\n|  power  |\n===========\n");
+	}
+
+}
