8f0d927fce943eccc7a7c01d6c577e4e77510f87
dboychev <dboychev95@gmail.com>
2016-09-08 09:45:11 +0300

    Update
    PP 1.8 updated.

--- Chapter 1/PP 1.8.java
+++ Chapter 1/PP 1.8.java
@@ -1,18 +1,17 @@
-
 public class Test 
 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("JJJJJJJJJJJJJJJ   AAAAAAAA    LLLL");
-		System.out.println("JJJJJJJJJJJJJJJ  AAAAAAAAAA   LLLL");
-		System.out.println("        JJJJ     AAA    AAA   LLLL");
-		System.out.println("        JJJJ     AAA    AAA   LLLL");
-		System.out.println("        JJJJ     AAAAAAAAAA   LLLL");
-		System.out.println("J       JJJJ     AAAAAAAAAA   LLLL");
-		System.out.println("JJ      JJJJ     AAA    AAA   LLLL");
-		System.out.println(" JJJJJJJJJJJ     AAA    AAA   LLLLLLLLLLLLLL");
-		System.out.println("  JJJJJJJJJ      AAA    AAA   LLLLLLLLLLLLLL");
+		System.out.println("DDDDDDDD     IIIIIIIIII   BBBBBBBB");
+		System.out.println("DDDDDDDDD    IIIIIIIIII   BBBBBBBBB");
+		System.out.println("DDD    DDD     IIIIII     BBB    BBB");
+		System.out.println("DDD     DDD    IIIIII     BBBBBBBBBB");
+		System.out.println("DDD     DDD    IIIIII     BBBBBBBBB");
+		System.out.println("DDD     DDD    IIIIII     BBBBBBBBBB");
+		System.out.println("DDD    DDD     IIIIII     BBB    BBB");
+		System.out.println("DDDDDDDDD    IIIIIIIIII   BBBBBBBBB");
+		System.out.println("DDDDDDDD     IIIIIIIIII   BBBBBBBB");
 	}
 
 }
