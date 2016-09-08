78d66e6f3faa6be6139eb84e842eae1f50f0f75d
dboychev <dboychev95@gmail.com>
2016-09-08 19:50:53 +0300

    Program updated
    PP 2.10 updated - added side's length condition: > 0.

--- Chapter 2/PP 2.10.java
+++ Chapter 2/PP 2.10.java
@@ -5,9 +5,12 @@ public class Test
 	public static void main(String[] args) 
 	{
 		Scanner scan = new Scanner (System.in);
-		int side;
-		System.out.print("Enter the length of the square's side: ");
-		side = scan.nextInt();
+		int side = 0;
+		while (side <= 0)
+		{
+			System.out.print("Enter the length of the square's side: ");
+			side = scan.nextInt();
+		}
 		System.out.println("Perimeter of the square: " + (side * 4));
 		System.out.println("Area of the square: " + (side * side));
 	}
