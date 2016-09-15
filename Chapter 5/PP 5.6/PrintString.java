af4836bfd901b842241b1dee48e53c61751c32ec
dboychev <dboychev95@gmail.com>
2016-09-15 16:03:40 +0300

    First PP added
    Chapter 5: PP 5.6 added.

---
+++ Chapter 5/PP 5.6/PrintString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintString 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner (System.in); //Creating Scanner object for reading the input
+		System.out.print("Enter string: "); //Printing an "Enter string: " message
+		String str = scan.nextLine(); //Creating a string 'str', assigning the input to it
+		
+		for (int i = 0; i < str.length(); i++) //A loop from 0 to the number of the length of 'str'
+		{
+			System.out.println(str.charAt(i)); //Printing each character of 'str'
+		}
+	}
+
+}
