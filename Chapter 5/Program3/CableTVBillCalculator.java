4ba29f06bb82e4247671fd1cac47430d9c2b5bd6
dboychev <dboychev95@gmail.com>
2016-09-17 09:19:44 +0300

    Comments
    Chapter 5: Program3 comments added.

--- Chapter 5/Program3/CableTVBillCalculator.java
+++ Chapter 5/Program3/CableTVBillCalculator.java
@@ -5,7 +5,7 @@ public class CableTVBillCalculator
 {
 	public static int computeBasic(boolean hasDVRBox)
 	{
-		if (!hasDVRBox)
+		if (!hasDVRBox) //If there is no DVRBox
 		{
 			return 3500;
 		}
@@ -18,7 +18,7 @@ public class CableTVBillCalculator
 	
 	public static int computeFamily(int premiumChannels, boolean hasDVRBox)
 	{
-		if (!hasDVRBox)
+		if (!hasDVRBox)  //If there is no DVRBox
 		{
 			return 5000 + premiumChannels * 799; 
 		}
@@ -31,9 +31,9 @@ public class CableTVBillCalculator
 	
 	public static int computePlatinum(int premiumChannels, int numMovies, boolean hasDVRBox)
 	{
-		if (!hasDVRBox)
+		if (!hasDVRBox)  //If there is no DVRBox
 		{
-			if (numMovies > 4)
+			if (numMovies > 4) //If pay-per-view movies are more than 4
 			{
 				return 7250 + premiumChannels * 549 + 4 * 499 + (numMovies - 4) * 249; 
 			}
@@ -46,7 +46,7 @@ public class CableTVBillCalculator
 		
 		else
 		{
-			if (numMovies > 4)
+			if (numMovies > 4) //If pay-per-view movies are more than 4
 			{
 				return (int) (1.1 * (7250 + premiumChannels * 549 + 4 * 499 + (numMovies - 4) * 249)); 
 			}
@@ -60,28 +60,28 @@ public class CableTVBillCalculator
 	
 	public static void main(String[] args) 
 	{
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in); //Creating an object for reading input
 		System.out.println("--Cable Service Menu--");
 		System.out.println("BAS [Basic Cable]");
 		System.out.println("FAM [Family Cable]");
 		System.out.println("PLA [Platinum Cable]");
 		System.out.print("Enter your menu choice: ");
-		DecimalFormat fmt = new DecimalFormat("#.00");
+		DecimalFormat fmt = new DecimalFormat("#.00"); //Creating an object for formatting the price number
 		
-		boolean DVRB = false;
-		String DVRBox = "";
+		boolean DVRB = false; //A boolean that says if the user has a DVRBox
+		String DVRBox = ""; //A string that says if the user has a DVRBox
 		
 		String choice = scan.nextLine();
-		if (!choice.equals("BAS") && !choice.equals("FAM") && !choice.equals("PLA"))
+		if (!choice.equals("BAS") && !choice.equals("FAM") && !choice.equals("PLA")) //If the input is invalid
 		{
 			System.out.println("Cannot compute cable bill - unknown service type.");
 		}
 		
 		else
 		{
-			System.out.print("Do you have a DVR Box? (YES/NO): ");
+			System.out.print("Do you have a DVR Box? (YES/NO): "); //Asking the user if he has a DVR Box
 			DVRBox = scan.nextLine();
-			if (!DVRBox.equals("YES") && !DVRBox.equals("NO"))
+			if (!DVRBox.equals("YES") && !DVRBox.equals("NO")) //If answer is neither 'YES', nor 'NO'
 			{
 				System.out.println("Warning: Using an answer of NO.");
 			}
@@ -89,10 +89,10 @@ public class CableTVBillCalculator
 		
 		if (DVRBox.equals("YES"))
 		{
-			DVRB = true;
+			DVRB = true; //Changing the value of DVRB boolean if the answer is 'YES'
 		}
 		
-		if (choice.equals("BAS"))
+		if (choice.equals("BAS")) //Computing the bill of the user depending on the service
 		{	
 			System.out.println("Your cable bill is $" + fmt.format((float)computeBasic(DVRB) / 100));
 		}
@@ -113,7 +113,7 @@ public class CableTVBillCalculator
 			System.out.println("Your cable bill is $" + fmt.format((float)computePlatinum(premium, ppviews, DVRB) / 100));
 		}
 		
-		scan.close();
+		scan.close(); //Closing the scanner
 	}
 
 }
