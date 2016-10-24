a2a6d4b7303d477899f61e2704a6b23dc49533d0
dboychev <dboychev95@gmail.com>
2016-10-24 09:59:21 +0300

    New project
    Chapter 10: PP 10.2 added.

---
+++ Chapter 10/PP 10.2/StringTooLongException.java
@@ -0,0 +1,8 @@
+
+public class StringTooLongException extends Exception
+{
+	public StringTooLongException(String text) //Constructor of the class
+	{
+		super(text); //Calling the 'Exception' class' constructor
+	}
+}
---
+++ Chapter 10/PP 10.2/StringTooLongExceptionDriver.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class StringTooLongExceptionDriver 
+{
+
+	public static void main(String[] args) throws StringTooLongException
+	{
+		final int MAX = 20; //A constant value for maximum string length
+		Scanner scan = new Scanner(System.in); //An object for reading input
+		String words = ""; //A string where the text will be kept
+
+		//An object for the exception
+		StringTooLongException problem = new StringTooLongException("String entered is too long!");
+		
+		//A loop for entering strings until the string is 'DONE'
+		while (!words.equals("DONE"))
+		{
+			try
+			{
+				System.out.print("Enter string: ");
+				words = scan.nextLine();
+				
+				if (words.length() > 20) //If the string length is bigger than 20, the exception is thrown
+				{
+					throw problem;
+				}
+			}
+			
+			catch (StringTooLongException e) //Catching the exception and printing the message
+			{
+				System.out.println(e.getMessage());
+			}
+			
+			//The while loop continues until 'DONE' is typed
+		}
+		
+		System.out.println("End of main method.");
+		
+		scan.close();
+	}
+
+}
